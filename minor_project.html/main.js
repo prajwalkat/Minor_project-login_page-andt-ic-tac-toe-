56b72802171c4eb90ff4ce353c59a2aa323344a4
prajwalkat <69921356+prajwalkat@users.noreply.github.com>
2022-07-19 10:48:38 +0530

    Add files via upload

---
+++ minor_project.html/main.js
@@ -0,0 +1,12 @@
+
+
+var playAgainButton = document.querySelector("#next_page")
+var UN = document.querySelector("#un")
+var PW = document.querySelector("#pw")
+
+
+playAgainButton.onclick = () => {
+    
+    location.href = "./next_page.html"
+   
+}
